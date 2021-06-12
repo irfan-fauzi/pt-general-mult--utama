9d6416cf54a8d04ddb247c35505c3898808d009f
irfan-fauzi <irfanwintech@yahoo.com>
2021-06-12 22:14:59 +0700

    infra bisa di click

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -77,16 +77,109 @@ tabProperty.forEach(el => {
   })
 })
 
-
-const satu = document.querySelector('#satu')
-const galery = satu.querySelectorAll('.container .card')
-const previewBox = document.querySelector('.preview-box');
-const previewImg = previewBox.querySelector('img');
-const closeIcon = document.querySelector('.close');
+// ----------------------Industry-------------------------------
+// Precast
+const precast = document.querySelector('#satu');
+const galeryPrecast = precast.querySelectorAll('.container .card')
+const previewBoxPrecast = precast.querySelector('.preview-box');
+const previewImgPrecast = previewBoxPrecast.querySelector('img');
+const closeIconPrecast = precast.querySelector('.close');
 const shadow = document.querySelector('.shadow');
+const prevPrecast = previewBoxPrecast.querySelector('.prev');
+const nextPrecast = previewBoxPrecast.querySelector('.next');
+
+// fasility
+const industrialFacility = document.querySelector('#dua');
+const galeryIndustrial = industrialFacility.querySelectorAll('.container .card');
+const previewBoxIndustrial = industrialFacility.querySelector('.preview-box');
+const previewImgIndustrial = previewBoxIndustrial.querySelector('img');
+const closeIconIndustrial = industrialFacility.querySelector('.close');
+const prevIndustrial = previewBoxIndustrial.querySelector('.prev');
+const nextIndustrial = previewBoxIndustrial.querySelector('.next');
+
+// bitumen
+const bitumen = document.querySelector('#tiga');
+const galeryBitumen = bitumen.querySelectorAll('.container .card');
+const previewBoxBitumen = bitumen.querySelector('.preview-box');
+const previewImgBitumen = previewBoxBitumen.querySelector('img');
+const closeIconBitumen = bitumen.querySelector('.close');
+const prevBitumen = previewBoxBitumen.querySelector('.prev');
+const nextBitumen = previewBoxBitumen.querySelector('.next');
+
+// ------------------------ Energy and Industrial plant----
+
+// epc
+const epc = document.querySelector('#satu-2');
+const galeryEpc = epc.querySelectorAll('.container .card');
+const previewBoxEpc = epc.querySelector('.preview-box');
+const previewImgEpc = previewBoxEpc.querySelector('img');
+const closeIconEpc = epc.querySelector('.close');
+const prevEpc = previewBoxEpc.querySelector('.prev');
+const nextEpc = previewBoxEpc.querySelector('.next');
+
+// operation and maintance
+
+const operation = document.querySelector('#dua-2');
+const galeryOperation = operation.querySelectorAll('.container .card');
+const previewBoxOperation = operation.querySelector('.preview-box');
+const previewImgOperation = previewBoxOperation.querySelector('img');
+const closeIconOperation = operation.querySelector('.close');
+const prevOperation = previewBoxOperation.querySelector('.prev');
+const nextOperation = previewBoxOperation.querySelector('.next');
+
+// ------------------------- Infrastructure and Building ------
+
+// civil construction
+const civil = document.querySelector('#infra-1');
+const galeryCivil = civil.querySelectorAll('.container .card');
+const previewBoxCivil = civil.querySelector('.preview-box');
+const previewImgCivil = previewBoxCivil.querySelector('img');
+const closeIconCivil = civil.querySelector('.close');
+const prevCivil = previewBoxCivil.querySelector('.prev');
+const nextCivil = previewBoxCivil.querySelector('.next');
+
+// building constructor
+const building = document.querySelector('#infra-2');
+const galeryBuilding = building.querySelectorAll('.container .card');
+const previewBoxBuilding = building.querySelector('.preview-box');
+const previewImgBuilding = previewBoxBuilding.querySelector('img');
+const closeIconBuilding = building.querySelector('.close');
+const prevBuilding = previewBoxBuilding.querySelector('.prev');
+const nextBuilding = previewBoxBuilding.querySelector('.next');
+
+// steel construction
+const steel = document.querySelector('#infra-3');
+const galerySteel = steel.querySelectorAll('.container .card');
+const previewBoxSteel = steel.querySelector('.preview-box');
+const previewImgSteel = previewBoxSteel.querySelector('img');
+const closeIconSteel = steel.querySelector('.close');
+const prevSteel = previewBoxSteel.querySelector('.prev');
+const nextSteel = previewBoxSteel.querySelector('.next');
+
+
 
 
 window.onload = () => {
+
+  fungsiPreviewPrecast(galeryPrecast, previewBoxPrecast, previewImgPrecast, closeIconPrecast, shadow, prevPrecast, nextPrecast);
+
+  fungsiPreviewPrecast(galeryIndustrial, previewBoxIndustrial, previewImgIndustrial, closeIconIndustrial, shadow, prevIndustrial, nextIndustrial);
+
+  fungsiPreviewPrecast(galeryBitumen, previewBoxBitumen, previewImgBitumen, closeIconBitumen, shadow, prevBitumen, nextBitumen);
+
+  fungsiPreviewPrecast(galeryEpc, previewBoxEpc, previewImgEpc, closeIconEpc, shadow, prevEpc, nextEpc);
+  
+  fungsiPreviewPrecast(galeryOperation, previewBoxOperation, previewImgOperation, closeIconOperation, shadow, prevOperation, nextOperation);
+  
+  fungsiPreviewPrecast(galeryCivil, previewBoxCivil, previewImgCivil, closeIconCivil, shadow, prevCivil, nextCivil);
+
+  fungsiPreviewPrecast(galeryBuilding, previewBoxBuilding, previewImgBuilding, closeIconBuilding, shadow, prevBuilding, nextBuilding);
+
+  fungsiPreviewPrecast(galerySteel, previewBoxSteel, previewImgSteel, closeIconSteel, shadow, prevSteel, nextSteel);
+
+}
+
+const fungsiPreviewPrecast = (galery, previewBox, previewImg, closeIcon, shadow, prevBtn, nextBtn ) => {
   for(let i = 0; i < galery.length; i++){
     let newIndex = i;
     let clickImgIndex;
@@ -98,9 +191,6 @@ window.onload = () => {
         //console.log(selectedImgUrl)
         previewImg.src = selectedImgUrl
       }
-
-      const prevBtn = document.querySelector('.prev');
-      const nextBtn = document.querySelector('.next');
 
       if(newIndex == 0){
         prevBtn.style.display = "none";
@@ -147,3 +237,6 @@ window.onload = () => {
     }
   }
 }
+
+
+
